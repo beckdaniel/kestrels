011858aa852a3bb11b3dc9df27ca8d07e02fb672
Daniel Beck <Daniel Beck>
2015-01-14 17:33:08 +1100

    more definitions

--- tree/tree_kernel.hpp
+++ tree/tree_kernel.hpp
@@ -5,7 +5,9 @@
 #include <unordered_map>
 #include "tree.hpp"
 
-class KernelResult{
+typedef std::pair<int, int> IDPair;
+
+struct KernelResult{
   float k;
   std::vector<float> dlambda;
   std::vector<float> dsigma;
@@ -22,12 +24,15 @@ public:
   SymbolAwareSubsetTreeKernel(const std::vector<float> &lambda,
 			      const std::vector<float> &alpha, bool normalize);
   std::vector<float> Kdiag(const std::vector<std::string> &trees);
-  void compute_kernel(const NodeList &node_list1, const NodeList &node_list2,
+  void compute_kernel(const NodeList &nodes1, const NodeList &nodes2,
 		      KernelResult &kernel_result);
+
 
 private:
   void build_cache(const std::vector<std::string> &trees);
-
+  void delta(const IDPair &id_pair, const NodeList &nodes1,
+	     const NodeList &nodes2, double *delta_matrix,
+	     double *dlambda_tensor, double *dsigma_tensor);
 };
 
 #endif //TREE_KERNEL_H
